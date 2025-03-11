9e94257ece269c21bdec7c8f3d2d748f30e48976
kilianpaquier <kilian@kilianpaquier.com>
2025-03-11 12:43:50 +0000

    fix(scrolltop): add animation and fix bootstrap, lazysizes impact on custom js
    Signed-off-by: kilianpaquier <kilian@kilianpaquier.com>

--- assets/js/scrolltop.js
+++ assets/js/scrolltop.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const scrolltop = document.getElementById("btn-scrolltop")
-    const scrollclass = function () {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            scrolltop.classList.remove("d-none")
-        } else {
-            scrolltop.classList.add("d-none")
-        }
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrolltop.classList.replace("animate__zoomOut", "animate__zoomIn") || scrolltop.classList.add("animate__zoomIn")
+        scrolltop.classList.remove("d-none")
     }
 
-    scrollclass()
-    document.addEventListener("scroll", scrollclass)
+    document.addEventListener("scroll", () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrolltop.classList.replace("animate__zoomOut", "animate__zoomIn") || scrolltop.classList.add("animate__zoomIn")
+            scrolltop.classList.remove("d-none")
+        } else {
+            scrolltop.classList.replace("animate__zoomIn", "animate__zoomOut") || scrolltop.classList.add("animate__zoomOut")
+        }
+    })
+
+    scrolltop.addEventListener("animationend", () => {
+        if (scrolltop.classList.contains("animate__zoomOut")) {
+            scrolltop.classList.add("d-none")
+        }
+    })
+
     scrolltop.addEventListener("click", () => {
         document.body.scrollTop = 0
         document.documentElement.scrollTop = 0
